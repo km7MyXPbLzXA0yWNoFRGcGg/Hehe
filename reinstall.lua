f49a3112253e02be5f8afa3b88ef027946743c36
REDLINERNULLLLL <devilfruitpowersucktion@gmail.com>
2026-08-07 19:52:04 -0700

    Replace old repository references

--- reinstall.lua
+++ reinstall.lua
@@ -1,6 +1,6 @@
 local folderToClean = "newvape"
 local folderToKeep = "profiles"
-local reinstallUrl = "https://raw.githubusercontent.com/poopparty/poopparty/main/NewMainScript.lua"
+local reinstallUrl = "https://raw.githubusercontent.com/km7MyXPbLzXA0yWNoFRGcGg/Hehe/main/NewMainScript.lua"
 
 local function deleteRecursive(path, keepPath)
     if path == keepPath then return end
